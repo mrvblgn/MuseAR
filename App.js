c020be6e856b6506b480df68a1f12bc844e8779a
raodabas <116227620+raodabas@users.noreply.github.com>
2024-05-24 18:25:04 +0300

    guncellendi

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { ScaledSheet } from "react-native-size-matters"
-import { FavoritesProvider } from './src/context/FavoritesContext';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { ScaledSheet } from "react-native-size-matters";
+import { FavoritesProvider } from "./src/context/FavoritesContext";
 
 // screens
-import SplashScreen from './src/screens/SplashScreen';
-import LocationScreen from './src/screens/LocationScreen';
-import ARCamera from './src/screens/ARCamera';
-import Profile from './src/screens/Profile';
-import GalleryStack from './src/GalleryStack';
-import Login from './src/screens/Login';
-import Signup from './src/screens/Signup';
-
+import SplashScreen from "./src/screens/SplashScreen";
+import LocationScreen from "./src/screens/LocationScreen";
+import ARCamera from "./src/screens/ARCamera";
+import Profile from "./src/screens/Profile";
+import GalleryStack from "./src/GalleryStack";
+import Login from "./src/screens/Login";
+import Signup from "./src/screens/Signup";
+import Muze1 from "./src/screens/Galeri/BandirmaMuze";
+import MuzeIstArk from "./src/screens/Galeri/İstanbulArkeoloji";
+import MuzeSanArk from "./src/screens/Galeri/SanliurfaArkeoloji";
+import MuzeTopk from "./src/screens/Galeri/TopkapiSarayi";
+import ContentDetail from "./src/screens/Galeri/ContentDetail";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -28,25 +32,24 @@ function AuthStackNavigator() {
   );
 }
 
-
 function TabNavigator() {
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
         headerShown: false,
-        tabBarStyle: { 
+        tabBarStyle: {
           ...styles.tabBarStyle,
         },
-        tabBarActiveTintColor: '#218DF0',
+        tabBarActiveTintColor: "#218DF0",
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
 
-          if (route.name === 'Galeri') {
-            iconName = focused ? 'images' : 'images-outline';
-          } else if (route.name === 'AR Kamera') {
-            iconName = focused ? 'camera' : 'camera-outline';
-          } else if (route.name === 'Profil') {
-            iconName = focused ? 'person' : 'person-outline';
+          if (route.name === "Galeri") {
+            iconName = focused ? "images" : "images-outline";
+          } else if (route.name === "AR Kamera") {
+            iconName = focused ? "camera" : "camera-outline";
+          } else if (route.name === "Profil") {
+            iconName = focused ? "person" : "person-outline";
           }
 
           return <Ionicons name={iconName} size={size} color={color} />;
@@ -64,11 +67,18 @@ export default function App() {
   return (
     <FavoritesProvider>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false, animation: 'none' }}>
+        <Stack.Navigator
+          screenOptions={{ headerShown: false, animation: "none" }}
+        >
           <Stack.Screen name="SplashScreen" component={SplashScreen} />
           <Stack.Screen name="AuthStack" component={AuthStackNavigator} />
           <Stack.Screen name="LocationScreen" component={LocationScreen} />
           <Stack.Screen name="TabNavigator" component={TabNavigator} />
+          <Stack.Screen name="Muze1" component={Muze1} />
+          <Stack.Screen name="MuzeIstArk" component={MuzeIstArk} />
+          <Stack.Screen name="MuzeSanArk" component={MuzeSanArk} />
+          <Stack.Screen name="MuzeSanTopk" component={MuzeTopk} />
+          <Stack.Screen name="ContentDetail" component={ContentDetail} />
         </Stack.Navigator>
       </NavigationContainer>
     </FavoritesProvider>
@@ -78,16 +88,16 @@ export default function App() {
 const styles = ScaledSheet.create({
   tabBarStyle: {
     borderTopWidth: 0,
-    borderTopLeftRadius: '25@s',
-    borderTopRightRadius: '25@s',
-    position: 'absolute',
+    borderTopLeftRadius: "25@s",
+    borderTopRightRadius: "25@s",
+    position: "absolute",
     shadowColor: "gray",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.5,
-        shadowRadius: 3.84,
-        elevation: 5,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
-})
+});
