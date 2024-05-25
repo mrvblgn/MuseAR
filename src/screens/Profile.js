1ede9b5b5329d6b32db01a06d70625d389284474
Elanur Birinci <elanurrbirinci@gmail.com>
2024-05-25 14:40:06 +0300

    update

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
 import { useFavorites } from '../context/FavoritesContext';
-import { ScaledSheet } from "react-native-size-matters";
 
 const Profile = () => {
   const { favorites } = useFavorites();
@@ -24,14 +23,29 @@ const Profile = () => {
   );
 };
 
-const styles = ScaledSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 16,
   },
   title: {
-    fontSize: "20@s",
-    fontWeight: "bold",
-    marginTop: "100@s",
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  itemContainer: {
+    marginBottom: 16,
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
+  itemTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  itemDescription: {
+    fontSize: 14,
+    color: '#666',
   },
 });
 
